58bb88fd4a60a561cbd11de9b68ad8b9ab58a1a2
chunmu.zhang@dji.com <chunmu.zhang@dji.com>
2025-02-05 15:32:17 +0800

    feat: 添加git别名脚本

---
+++ git-alias.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+git config --global alias.st status;
+git config --global alias.br branch;
+git config --global alias.pl pull;
+git config --global alias.ps push;
+git config --global alias.co checkout;
+git config --global alias.fc fetch;
+git config --global alias.a add;
+
+echo "git 命令别名配置完成"
